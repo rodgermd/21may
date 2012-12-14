409d164a66c3b9845a234453f6485a268a2cc3af
Alexander Murashov <rodger@ladela.com>
2012-12-14 23:13:18 +0200

    Fonts and fixtures

--- src/Site/BaseBundle/DataFixtures/ORM/LoadAccommodationData.php
+++ src/Site/BaseBundle/DataFixtures/ORM/LoadAccommodationData.php
@@ -14,21 +14,43 @@ class LoadAccommodationData implements FixtureInterface
    */
   public function load(ObjectManager $manager)
   {
-    $accommodation1 = new Accommodation();
-    $accommodation1
-      ->setTitle('title en')
-      ->setDescription('description en')
-      ->setSecondaryText('secondary_text en')
+    $estancia_perales = new Accommodation();
+    $estancia_perales
+      ->setTitle('Estancia Perales')
+      ->setDescription($this->load_file('estancia_perales1_en'))
+      ->setSecondaryText($this->load_file('estancia_perales2_en'))
       ->setTranslatableLocale('en')
-      ->addTranslation(new Translation('en', 'title', 'title en'))
-      ->addTranslation(new Translation('en', 'description', 'description en'))
-      ->addTranslation(new Translation('en', 'secondary_text', 'secondary_text en'))
-      ->addTranslation(new Translation('es', 'title', 'title es'))
-      ->addTranslation(new Translation('es', 'description', 'description es'))
-      ->addTranslation(new Translation('es', 'secondary_text', 'secondary_text es'))
+      ->addTranslation(new Translation('en', 'title', 'Estancia Perales'))
+      ->addTranslation(new Translation('en', 'description', $this->load_file('estancia_perales1_en')))
+      ->addTranslation(new Translation('en', 'secondary_text', $this->load_file('estancia_perales2_en')))
+      ->addTranslation(new Translation('es', 'title', 'Estancia Perales'))
+      ->addTranslation(new Translation('es', 'description', $this->load_file('estancia_perales1_es')))
+      ->addTranslation(new Translation('es', 'secondary_text', $this->load_file('estancia_perales2_es')))
     ;
 
-    $manager->persist($accommodation1);
+    $manager->persist($estancia_perales);
+
+    $montes_balmaceda = new Accommodation();
+    $montes_balmaceda
+      ->setTitle('Montes Balmaceda')
+      ->setDescription($this->load_file('montes_balmaceda1_en'))
+      ->setSecondaryText($this->load_file('montes_balmaceda2_en'))
+      ->setTranslatableLocale('en')
+      ->addTranslation(new Translation('en', 'title', 'Montes Balmaceda'))
+      ->addTranslation(new Translation('en', 'description', $this->load_file('montes_balmaceda1_en')))
+      ->addTranslation(new Translation('en', 'secondary_text', $this->load_file('montes_balmaceda2_en')))
+      ->addTranslation(new Translation('es', 'title', 'Montes Balmaceda'))
+      ->addTranslation(new Translation('es', 'description', $this->load_file('montes_balmaceda1_es')))
+      ->addTranslation(new Translation('es', 'secondary_text', $this->load_file('montes_balmaceda2_es')))
+    ;
+    $manager->persist($montes_balmaceda);
+
+
     $manager->flush();
+  }
+
+  protected function load_file($name)
+  {
+    return file_get_contents(__DIR__ . "/html/$name.html");
   }
 }
