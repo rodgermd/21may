47afd922f930477e2f81d04e1d221b1a0ca355ce
Alexander Murashov <rodger@ladela.com>
2013-02-19 19:56:28 +0200

    #35 ad language slector

--- src/Site/BaseBundle/Controller/LanguageController.php
+++ src/Site/BaseBundle/Controller/LanguageController.php
@@ -5,6 +5,8 @@ namespace Site\BaseBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Bundle\FrameworkBundle\Routing\Router;
+use Symfony\Component\HttpFoundation\Request;
 
 class LanguageController extends Controller
 {
@@ -15,5 +17,20 @@ class LanguageController extends Controller
   {
     $locale = $this->getRequest()->getLocale();
     return $this->redirect($this->generateUrl('homepage', array('_locale' => $locale)));
+  }
+
+  /**
+   * @Route("/switch-to")
+   * @Template
+   */
+  public function switchAction($route, $route_params, $current)
+  {
+    $languages = $this->container->getParameter('languages');
+    unset($languages[$current]);
+    $available_locales = array_keys($languages);
+    $target_locale = $available_locales[0];
+    $route_params['_locale'] = $target_locale;
+
+    return compact('route', 'route_params', 'target_locale');
   }
 }
