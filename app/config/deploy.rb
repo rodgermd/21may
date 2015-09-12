0b2ad753115875d67016a600bca4d0b5338b5334
rodger <rodger@mura-show.com>
2015-09-12 16:30:55 +0300

    Fix deploy scripts

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -1,4 +1,4 @@
-set :application, "21 may"
+set :application, "21may"
 set :domain,      "ec2-52-27-153-196.us-west-2.compute.amazonaws.com"
 set :deploy_to,   "/var/www/21may"
 set :app_path,    "app"
@@ -10,12 +10,13 @@ set :model_manager, "doctrine"
 
 role :web,        domain                         # Your HTTP server, Apache/etc
 role :app,        domain, :primary => true       # This may be the same as your `Web` server
+role :db,         domain, :primary => true       # This may be the same as your `Web` server
 
 set  :keep_releases,  10
 
 set :keep_releases,   10
 set :shared_children, [app_path + "/logs", app_path + "/cache/sessions", web_path + "/uploads"]
-set :shared_files,    ["app/config/parameters.yml"]
+set :shared_files,    ["app/config/parameters.yml", "app/config/parameters.private.yml"]
 set :use_composer,    true
 set :update_vendors,  false
 set :use_sudo,        true
@@ -26,6 +27,6 @@ set :webserver_user,    "www-data"
 set :permission_method, :acl
 
 set :dump_assetic_assets, true
-set :interactive_mode, false
+set :interactive_mode, true
 
 logger.level = Logger::MAX_LEVEL
